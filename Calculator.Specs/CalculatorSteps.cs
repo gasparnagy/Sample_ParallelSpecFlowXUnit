b75990b21918ecd2767014395f580404571fbc0f
Gaspar Nagy <gaspar.nagy@gmail.com>
2016-02-11 12:34:11 +0100

    Access ScenarioContext with context injection in order to be able to run the tests parallel

--- Calculator.Specs/CalculatorSteps.cs
+++ Calculator.Specs/CalculatorSteps.cs
@@ -7,34 +7,41 @@ namespace Calculator.Specs
     [Binding]
     public class CalculatorSteps
     {
+        private readonly ScenarioContext scenarioContext;
+
+        public CalculatorSteps(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
         [BeforeScenario]
         public void InitializeCalculator()
         {
-            ScenarioContext.Current["calc"] = new Calculator();
+            scenarioContext["calc"] = new Calculator();
         }
 
         [Given(@"I have entered (.*) into the calculator")]
         public void GivenIHaveEnteredIntoTheCalculator(int operand)
         {
-            ((Calculator)ScenarioContext.Current["calc"]).Enter(operand);
+            ((Calculator)scenarioContext["calc"]).Enter(operand);
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
-            ((Calculator)ScenarioContext.Current["calc"]).Add();
+            ((Calculator)scenarioContext["calc"]).Add();
         }
 
         [When(@"I press multiply")]
         public void WhenIPressMultiply()
         {
-            ((Calculator)ScenarioContext.Current["calc"]).Multiply();
+            ((Calculator)scenarioContext["calc"]).Multiply();
         }
 
         [Then(@"the result should be (.*) on the screen")]
         public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
         {
-            Assert.Equal(expectedResult, ((Calculator)ScenarioContext.Current["calc"]).Result);
+            Assert.Equal(expectedResult, ((Calculator)scenarioContext["calc"]).Result);
         }
     }
 }
