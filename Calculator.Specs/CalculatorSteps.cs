38177075f2b459c1a2c05d869bfc122336f9e696
Gaspar Nagy <gaspar.nagy@gmail.com>
2016-02-11 12:14:47 +0100

    Store calculator in ScenarioContext.Current

--- Calculator.Specs/CalculatorSteps.cs
+++ Calculator.Specs/CalculatorSteps.cs
@@ -7,30 +7,34 @@ namespace Calculator.Specs
     [Binding]
     public class CalculatorSteps
     {
-        private readonly Calculator calculator = new Calculator();
+        [BeforeScenario]
+        public void InitializeCalculator()
+        {
+            ScenarioContext.Current["calc"] = new Calculator();
+        }
 
         [Given(@"I have entered (.*) into the calculator")]
         public void GivenIHaveEnteredIntoTheCalculator(int operand)
         {
-            calculator.Enter(operand);
+            ((Calculator)ScenarioContext.Current["calc"]).Enter(operand);
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
-            calculator.Add();
+            ((Calculator)ScenarioContext.Current["calc"]).Add();
         }
 
         [When(@"I press multiply")]
         public void WhenIPressMultiply()
         {
-            calculator.Multiply();
+            ((Calculator)ScenarioContext.Current["calc"]).Multiply();
         }
 
         [Then(@"the result should be (.*) on the screen")]
         public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
         {
-            Assert.Equal(expectedResult, calculator.Result);
+            Assert.Equal(expectedResult, ((Calculator)ScenarioContext.Current["calc"]).Result);
         }
     }
 }
